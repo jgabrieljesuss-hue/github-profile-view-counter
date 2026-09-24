54db8f97d86642309401b3e33a0e649f2884fc23
José Gabriel <jgabrieljesuss@gmail.com>
2026-09-23 23:02:42 -0300

    Add eyePath SVG path definition

--- api/index.js
+++ api/index.js
@@ -84,6 +84,7 @@ module.exports = async (req, res) => {
   const padY       = isNobg ? 0 : 7;
   const iconGap    = showIcon ? 5 : 0;
 
+    const eyePath = `M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zm0 12.5c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z`;
 
   const labelTW  = showLabel ? labelText : '';
   const fullText = labelTW + countStr;
